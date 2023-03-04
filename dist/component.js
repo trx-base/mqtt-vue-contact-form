e2acb6b8633331f6a5ee4e08342cb6ea5077b52b
Te Muks <te.muks@lotusit.ba>
2023-03-05 00:24:19 +0100

    Component supports properties.

--- dist/component.js
+++ dist/component.js
@@ -1,20 +1,27 @@
- Vue.createApp({ 
-    template: `
-      <div>
-       <h1>mqtt-vue-contact-form</h1>
-       <p>{{ message }}</p>
-      </div>
-    `,
-    setup() {
-        const message = 'Hello, here is the composite speaking!'
-    
-        // expose to template and other options API hooks
-        return {
-            message
-        }
-      },
-    
-      mounted() {
-        console.log("MqttVueContactForm mounted.") // 0
-      }
+const Component = {
+  template: `
+  <div>
+   <h1>mqtt-vue-contact-form</h1>
+   <p>{{ message }}</p>
+  </div>
+`,
+props: {
+  title: String
+},
+setup(props) {
+    const message = props.title + ' - Howdy, here is the composite speaking!'
+
+    // expose to template and other options API hooks
+    return {
+        message
+    }
+  },
+
+  mounted() {
+    console.log("MqttVueContactForm") // 0
+  }
+}
+ 
+Vue.createApp({ 
+  components: { Component }
 }).mount("#MqttVueContactForm");
