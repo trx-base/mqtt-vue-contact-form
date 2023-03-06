66cf0cb5ba4f736237c1b651c7f9e63e8db57334
Mak Bukovac <mak.bukovac@lotusit.ba>
2023-03-07 00:20:57 +0100

    Not using composition api for methods.

--- dist/component.js
+++ dist/component.js
@@ -1,24 +1,21 @@
 export default {
-  template: `
-  <div>
-    <h2>component.js</h2>
-    <slot :submit="submit"/>
-  </div>`,
+  template: `<slot :submit="onSubmit"/>`,
   props: {
     mqttHost: String
   },
   setup () {
-  
-    const submit = (fullName, email, message) => {
+    return {
+      
+    };
+  },
+  methods: {
+    onSubmit(fullName, email, message) {
       console.log("Submitting...")
       console.log("Name:" + fullName)
       console.log("Email:" + email)
       console.log("Message:" + message)
       console.log("---------------")
     }
-    return {
-      submit
-    };
   },
   mounted () {
     console.log('Component mounted');
