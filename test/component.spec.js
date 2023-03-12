e2074fb64c50254edbae38cab43b765bb36c8fb2
Mak Bukovac <mak.bukovac@lotusit.ba>
2023-03-13 00:00:30 +0100

    Added test.

--- test/component.spec.js
+++ test/component.spec.js
@@ -2,10 +2,18 @@ import { describe, it, vi, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import Component from '../dist/component';
 
+// Init Mocks
+const publish = vi.fn();
+const connect = vi.fn(() => {
+  return {
+    publish
+  };
+});
 const mqtt = {
-  connect: vi.fn()
+  connect
 };
 vi.stubGlobal('mqtt', mqtt);
+// Init Mocks End
 
 describe('mqtt-vue-contact-form', () => {
   it('should connect to mqtt when mounted', () => {
@@ -16,5 +24,15 @@ describe('mqtt-vue-contact-form', () => {
     });
 
     expect(mqtt.connect).toHaveBeenCalledWith('wss://expectedHost');
+  });
+
+  it('should publish to topic when submit', async () => {
+    const wrapper = mount(Component, {
+      propsData: {
+        mqttTopic: 'test/expected/topic'
+      }
+    });
+    wrapper.vm.submit();
+    expect(publish).toHaveBeenCalled();
   });
 });
